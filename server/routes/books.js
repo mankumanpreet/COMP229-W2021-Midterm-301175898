801c6dde8418413adad89d2a7b2a5904e9370d55
mankumanpreet <mankoo.manpreet04@gmail.com>
2021-03-07 21:34:18 +0530

    comments

--- server/routes/books.js
+++ server/routes/books.js
@@ -25,7 +25,7 @@ router.get('/', (req, res, next) => {
         books: books
       });
     }
-  });
+  }).sort("name");
 
 });
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,9 @@
+/* Book.js
+Manpreet Kaur ,
+Student Id-301175898
+07/03/2021
+COMP229-W2021-Midterm-301175898
+*/
 // modules required for routing
 let express = require('express');
 let router = express.Router();
@@ -10,7 +16,7 @@ let book = require('../models/books');
 router.get('/', (req, res, next) => {
   res.render('content/index', {
     title: 'Home',
-    books: ''
+    books: '/'
    });
 });
 
